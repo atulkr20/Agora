d146594575389795dec82ebf32635e3e96ec01d6
Kushagra Bharti <kushagrabharti04@gmail.com>
2026-05-18 18:14:57 +0530

    refactor(auth-model): clean up IUser and ISession interfaces by removing unnecessary line breaks

--- services/auth-service/src/model/auth.model.ts
+++ services/auth-service/src/model/auth.model.ts
@@ -7,39 +7,25 @@ export enum Role{
 
 export interface IUser {
     _id: ObjectId;
-
     email: string;
     username: string;
-
     password: string;
-
     role: Role;
-
     isBlocked: boolean;
-
     lastLoginAt?: Date;
-
     createdAt: Date;
     updatedAt: Date;
 }
 
 export interface ISession {
     _id: ObjectId;
-
     userId: ObjectId;
-
     refreshTokenHash: string; // hashed version of the refresh token
-
     ipAddress?: string; // like :- "192.168.1.1"
-
     userAgent?: string; //like :- chrome, mobile, etc.
-
     isRevoked: boolean;
-
     expiresAt: Date;
-
     createdAt: Date;
-
     updatedAt: Date;
 }
 
